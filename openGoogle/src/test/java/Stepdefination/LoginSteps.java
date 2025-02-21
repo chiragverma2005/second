f54f16aee6e2d88b51c8aba57bda98c094d54f0d
Chirag1709 <87745899+Chirag1709@users.noreply.github.com>
2025-02-21 14:29:32 +0530

    seconds

--- openGoogle/src/test/java/Stepdefination/LoginSteps.java
+++ openGoogle/src/test/java/Stepdefination/LoginSteps.java
@@ -8,7 +8,7 @@ public class LoginSteps {
 
 @Given("I want to write a step with precondition")
 public void i_want_to_write_a_step_with_precondition() {
-    System.out.println("I want to write a step with precondition");}
+    System.out.println("I want to write a step with precondition other");}
 
 @Given("some other precondition")
 public void some_other_precondition() {
